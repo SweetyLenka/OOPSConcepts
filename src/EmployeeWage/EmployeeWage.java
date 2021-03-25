e483ebcbe01b483fe01b92d8dc08d56cfc6a0f52
sweetylenka <sweetylenka2013@gmail.com>
2021-03-25 14:03:07 +0530

    UC1 - Check Employee is Present or Absent - Use ((RANDOM)) for Attendance Check

--- src/EmployeeWage/EmployeeWage.java
+++ src/EmployeeWage/EmployeeWage.java
@@ -1,12 +1,34 @@
 package EmployeeWage;
 
 public class EmployeeWage {
-	
-	
+	public static int Attendance()
+	{
+		int attendance= (int) Math.floor(Math.random()*10%2);
+		return attendance;
+		
+	}
+	public static void ComputeAttendance()
+	{
+		int empCheck=Attendance();
+		if(empCheck==1)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
+		
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("WECOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+		ComputeAttendance();
 		
 	}
 
 }
+
+	
+	
+	
