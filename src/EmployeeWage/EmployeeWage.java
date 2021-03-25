431cfa0a5b666822682649bdc78664a983ddf0b9
sweetylenka <sweetylenka2013@gmail.com>
2021-03-25 13:59:47 +0530

    Check Employee is Present or Absent - Use ((RANDOM)) for Attendance Check

---
+++ src/EmployeeWage/EmployeeWage.java
@@ -0,0 +1,12 @@
+package EmployeeWage;
+
+public class EmployeeWage {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WECOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+		
+	}
+
+}
