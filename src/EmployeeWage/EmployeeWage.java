73047b9149c3da72fb5fec796cf18cf4d8164e3b
sweetylenka <sweetylenka2013@gmail.com>
2021-03-25 15:25:00 +0530

    Add Part time Employee & Wage - Assume Part time Hour is 4

--- src/EmployeeWage/EmployeeWage.java
+++ src/EmployeeWage/EmployeeWage.java
@@ -1,36 +1,43 @@
 package EmployeeWage;
 
 public class EmployeeWage {
+	public static final int FULL_TIME=1;
+	public static final int PART_TIME=2;
 	public static final int WAGE_PER_HOUR=20;
 	public static final int FULL_DAY_HOUR=8;
+	public static final int HALF_DAY_HOUR=4;
 	public static int empWage=0;
 	
 	public static int Attendance()
 	{
-		int attendance= (int) Math.floor(Math.random()*10%2);
+		int attendance= (int) Math.floor(Math.random()*10%3);
 		return attendance;
 		
 	}
-	public static void ComputeEmpWage()
+	public static void ComputeEmpWageBasedOnHrs()
 	{
 		int empCheck=Attendance();
-		if(empCheck==1)
+		switch(empCheck)
 		{
+		case FULL_TIME:
 			empWage=WAGE_PER_HOUR*FULL_DAY_HOUR;
-			System.out.println("Employee Wage : "+empWage);
-		}
-		else
-		{
+			System.out.println("Employee Wage :" +empWage);
+			break;
+		case PART_TIME:
+			empWage=WAGE_PER_HOUR*HALF_DAY_HOUR;
+			System.out.println("Employee Wage :" +empWage);
+			break;
+		default:
 			System.out.println("Employee is absent");
+			
 		}
-		
+	
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		EmployeeWage empWage=new EmployeeWage();
-		empWage.ComputeEmpWage();
-		
+		empWage.ComputeEmpWageBasedOnHrs();
 	}
 
 }
